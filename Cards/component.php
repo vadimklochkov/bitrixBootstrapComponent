45d0785dfb89cad9cf3a5a1ac28bfea6770e5876
vadimklochkov <vadimklochkov@inbox.ru>
2021-05-21 16:33:48 +0300

    Добавление code в component.php (цветовой код)

--- Cards/component.php
+++ Cards/component.php
@@ -7,7 +7,7 @@ if(!Loader::includeModule("iblock"))
     return;
 }
 
-$arSelect = Array("ID", "NAME", "PREVIEW_PICTURE", "PREVIEW_TEXT", "DETAIL_TEXT", "DETAIL_PAGE_URL", "DATE_ACTIVE_FROM", "DATE_ACTIVE_TO", "PROPERTY_".$arParams["PROPERTY_CODE"]);
+$arSelect = Array("ID", "NAME", "PREVIEW_PICTURE", "PREVIEW_TEXT", "DETAIL_TEXT", "DETAIL_PAGE_URL", "DATE_ACTIVE_FROM", "DATE_ACTIVE_TO", "CODE", "PROPERTY_".$arParams["PROPERTY_CODE"]);
 $arFilter = Array("IBLOCK_ID" => IntVal($arParams["IBLOCK_ID"]), "ACTIVE"=>"Y", "PROPERTY_".$arParams["PROPERTY_CODE"]."_VALUE" => 'Да');
 $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
 
